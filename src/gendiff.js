598cc2e2692bf53a9e231f71430ff017728046ea
Konstantin <102475720+Konstantin-Gromakovskiy@users.noreply.github.com>
2024-09-13 13:28:27 +0300

    lint correction in gendiff.js and parsers.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -9,25 +9,17 @@ const genDiff = (filePath1, filePath2, formatter = 'stylish') => {
   const iter = (obj1, obj2) => {
     const keys1 = Object.keys(obj1);
     const keys2 = Object.keys(obj2);
-    const commonKeys = _.union(keys1, keys2).sort();
+    const commonKeys = [..._.union(keys1, keys2)].sort();
 
-    const objectWithDifferences = commonKeys.reduce((acc, key) => {
-      if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-        acc.push({ name: key, type: 'nested', children: iter(obj1[key], obj2[key]) });
-      } else if (!Object.hasOwn(obj1, key)) {
-        acc.push({ name: key, type: 'added', value: obj2[key] });
-      } else if (!Object.hasOwn(obj2, key)) {
-        acc.push({ name: key, type: 'removed', value: obj1[key] });
-      } else if (_.isEqual(obj1[key], obj2[key])) {
-        acc.push({ name: key, type: 'unchanged', value: obj1[key] });
-      } else {
-        acc.push({
-          name: key, type: 'updated', then: obj1[key], now: obj2[key],
-        });
-      }
-
-      return acc;
-    }, []);
+    const objectWithDifferences = commonKeys.map((key) => {
+      if (_.isObject(obj1[key]) && _.isObject(obj2[key])) { return { name: key, type: 'nested', children: iter(obj1[key], obj2[key]) }; }
+      if (!Object.hasOwn(obj1, key)) { return { name: key, type: 'added', value: obj2[key] }; }
+      if (!Object.hasOwn(obj2, key)) { return { name: key, type: 'removed', value: obj1[key] }; }
+      if (_.isEqual(obj1[key], obj2[key])) { return { name: key, type: 'unchanged', value: obj1[key] }; }
+      return {
+        name: key, type: 'updated', then: obj1[key], now: obj2[key],
+      };
+    });
     return objectWithDifferences;
   };
   const result = iter(data1, data2);
--- src/parsers.js
+++ src/parsers.js
@@ -5,15 +5,11 @@ import yaml from 'js-yaml';
 const getData = (filePath) => {
   const format = path.extname(filePath);
   const data = fs.readFileSync(filePath, 'utf-8');
-  let parse;
 
   if (format === '.yaml' || format === '.yml') {
-    parse = yaml.load(data, 'utf-8');
-  } else {
-    parse = JSON.parse(data);
+    return yaml.load(data, 'utf-8');
   }
-
-  return parse;
+  return JSON.parse(data);
 };
 
 export default getData;
