3427600156313903192cffba65282fa953c29cc0
Konstantin <102475720+Konstantin-Gromakovskiy@users.noreply.github.com>
2024-09-13 12:49:05 +0300

    lint correction in plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,46 +1,29 @@
 import _ from 'lodash';
 
 const plain = (objects) => {
+  const getAnswer = (value) => {
+    if (_.isObject(value)) { return '[complex value]'; }
+    if (_.isString(value)) { return `'${value}'`; }
+    return value;
+  };
+
   const iter = (currentValue, pathArray) => {
     if (!currentValue.children) {
-      const currentPathArray = [...pathArray];
-      currentPathArray.push(currentValue.name);
+      const currentPathArray = [...pathArray, currentValue.name];
       const path = currentPathArray.join('.');
       let answer;
-      let value;
-      let then;
-      let now;
-
-      if (_.isObject(currentValue.value)) {
-        value = '[complex value]';
-      } else if (_.isString(currentValue.value)) {
-        value = `'${currentValue.value}'`;
-      } else { value = currentValue.value; }
-
-      if (_.isObject(currentValue.then)) {
-        then = '[complex value]';
-      } else if (_.isString(currentValue.then)) {
-        then = `'${currentValue.then}'`;
-      } else { then = currentValue.then; }
-
-      if (_.isObject(currentValue.now)) {
-        now = '[complex value]';
-      } else if (_.isString(currentValue.now)) {
-        now = `'${currentValue.now}'`;
-      } else { now = currentValue.now; }
 
       if (currentValue.type === 'added') {
-        answer = `Property '${path}' was ${currentValue.type} with value: ${value}`;
+        return `Property '${path}' was ${currentValue.type} with value: ${getAnswer(currentValue.value)}`;
       } if (currentValue.type === 'removed') {
-        answer = `Property '${path}' was ${currentValue.type}`;
+        return `Property '${path}' was ${currentValue.type}`;
       } if (currentValue.type === 'updated') {
-        answer = `Property '${path}' was ${currentValue.type}. From ${then} to ${now}`;
+        return `Property '${path}' was ${currentValue.type}. From ${getAnswer(currentValue.then)} to ${getAnswer(currentValue.now)}`;
       }
       return answer;
     }
 
-    const currentPathArray = [...pathArray];
-    currentPathArray.push(currentValue.name);
+    const currentPathArray = [...pathArray, currentValue.name];
     const nextObjects = currentValue.children.map((child) => iter(child, currentPathArray));
     return nextObjects.filter(Boolean).join('\n');
   };
